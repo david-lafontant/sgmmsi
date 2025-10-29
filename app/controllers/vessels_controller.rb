70e6a6660769a59625baeb3fa1a8d61623e39232
david-lafontant <lafontant.david@gmail.com>
2025-10-29 19:50:00 -0400

    Assign current user id and mmsi id to vessel

--- app/controllers/vessels_controller.rb
+++ app/controllers/vessels_controller.rb
@@ -1,5 +1,5 @@
 class VesselsController < ApplicationController
-  before_action :set_vessel, only: %i[ show edit update destroy ]
+  before_action :set_vessel, only: %i[show edit update destroy]
 
   # GET /vessels or /vessels.json
   def index
@@ -22,10 +22,13 @@ class VesselsController < ApplicationController
   # POST /vessels or /vessels.json
   def create
     @vessel = Vessel.new(vessel_params)
-
+    @vessel.user_id = current_user.id
+    mmsi = @vessel.generate_mmsi
+    ref1 = Mmsi.create(mmsi_id: mmsi, user_id: current_user.id)
+    @vessel.mmsi_id = ref1.id
     respond_to do |format|
       if @vessel.save
-        format.html { redirect_to @vessel, notice: "Vessel was successfully created." }
+        format.html { redirect_to @vessel, notice: 'Vessel was successfully created.' }
         format.json { render :show, status: :created, location: @vessel }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +41,7 @@ class VesselsController < ApplicationController
   def update
     respond_to do |format|
       if @vessel.update(vessel_params)
-        format.html { redirect_to @vessel, notice: "Vessel was successfully updated.", status: :see_other }
+        format.html { redirect_to @vessel, notice: 'Vessel was successfully updated.', status: :see_other }
         format.json { render :show, status: :ok, location: @vessel }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,19 +55,22 @@ class VesselsController < ApplicationController
     @vessel.destroy!
 
     respond_to do |format|
-      format.html { redirect_to vessels_path, notice: "Vessel was successfully destroyed.", status: :see_other }
+      format.html { redirect_to vessels_path, notice: 'Vessel was successfully destroyed.', status: :see_other }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_vessel
-      @vessel = Vessel.find(params.expect(:id))
-    end
 
-    # Only allow a list of trusted parameters through.
-    def vessel_params
-      params.expect(vessel: [ :registration_number, :operation_area, :last_name, :first_name, :company_name, :email, :telephone, :mmsi_id, :user_id, documents: [] ])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_vessel
+    @vessel = Vessel.find(params.expect(:id))
+  end
+
+  # Only allow a list of trusted parameters through.
+  def vessel_params
+    params.expect(vessel: [:registration_number, :operation_area, :last_name, :first_name, :company_name, :email, :telephone, {
+                    documents: []
+                  }])
+  end
 end
