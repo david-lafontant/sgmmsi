d04cf32b5d8e08f992cee90ab45ff8ba4f6d0f6d
david-lafontant <lafontant.david@gmail.com>
2025-10-29 22:00:57 -0400

    Add user id and mmsi id to create  station

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -1,5 +1,5 @@
 class StationsController < ApplicationController
-  before_action :set_station, only: %i[ show edit update destroy ]
+  before_action :set_station, only: %i[show edit update destroy]
 
   # GET /stations or /stations.json
   def index
@@ -22,10 +22,14 @@ class StationsController < ApplicationController
   # POST /stations or /stations.json
   def create
     @station = Station.new(station_params)
+    @station.user_id = current_user.id
+    mmsi = @station.generate_station_mmsi(@station.station_type_id)
+    ref1 = Mmsi.create(mmsi_id: mmsi, user_id: current_user.id, category: 'station')
+    @station.mmsi_id = ref1.id
 
     respond_to do |format|
       if @station.save
-        format.html { redirect_to @station, notice: "Station was successfully created." }
+        format.html { redirect_to @station, notice: 'Station was successfully created.' }
         format.json { render :show, status: :created, location: @station }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +42,7 @@ class StationsController < ApplicationController
   def update
     respond_to do |format|
       if @station.update(station_params)
-        format.html { redirect_to @station, notice: "Station was successfully updated.", status: :see_other }
+        format.html { redirect_to @station, notice: 'Station was successfully updated.', status: :see_other }
         format.json { render :show, status: :ok, location: @station }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,19 +56,21 @@ class StationsController < ApplicationController
     @station.destroy!
 
     respond_to do |format|
-      format.html { redirect_to stations_path, notice: "Station was successfully destroyed.", status: :see_other }
+      format.html { redirect_to stations_path, notice: 'Station was successfully destroyed.', status: :see_other }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_station
-      @station = Station.find(params.expect(:id))
-    end
 
-    # Only allow a list of trusted parameters through.
-    def station_params
-      params.expect(station: [ :registration_number, :latitude, :longitude, :municipality, :station_type_id, :last_name, :first_name, :company_name, :email, :telephone, :mmsi_id, :user_id, documents: [] ])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_station
+    @station = Station.find(params.expect(:id))
+  end
+
+  # Only allow a list of trusted parameters through.
+  def station_params
+    params.expect(station: [:registration_number, :latitude, :longitude, :municipality, :station_type_id, :last_name, :first_name, :company_name, :email,
+                            :telephone, { documents: [] }])
+  end
 end
