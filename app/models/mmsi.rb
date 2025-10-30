a75be0fe036bdc8492fb6384883cab3e5ac1a978
david-lafontant <lafontant.david@gmail.com>
2025-10-29 22:02:33 -0400

    Setup one to one relation between mmsi and station and add new validations

--- app/models/mmsi.rb
+++ app/models/mmsi.rb
@@ -1,5 +1,8 @@
 class Mmsi < ApplicationRecord
   belongs_to :user
   has_one :vessel
-  validates :mmsi_id, presence: true
+  has_one :station
+  validates :mmsi_id, presence: true, uniqueness: true
+
+  validates :category, inclusion: { in: %w[station vessel], message: 'invalid data' }
 end
