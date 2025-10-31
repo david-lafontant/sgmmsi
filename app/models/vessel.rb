e6f770a81c2c85259716a693d297ceed7a4c0cc7
david-lafontant <lafontant.david@gmail.com>
2025-10-31 11:18:46 -0400

    Create display Mmsi method in vessel model

--- app/models/vessel.rb
+++ app/models/vessel.rb
@@ -13,4 +13,8 @@ class Vessel < ApplicationRecord
   def generate_vessel_mmsi
     "003290#{SecureRandom.random_number(10**5).to_s.rjust(5, '0')}"
   end
+
+  def display_mmsi(id)
+    Mmsi.find(id).mmsi_id
+  end
 end
