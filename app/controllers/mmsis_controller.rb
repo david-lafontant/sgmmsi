970fb45f2500f79a63e3e37ae3f7dbc6cd50519c
david-lafontant <lafontant.david@gmail.com>
2025-10-31 21:05:19 -0400

    Create controller for mmsi with edit , update, and delete action

---
+++ app/controllers/mmsis_controller.rb
@@ -0,0 +1,41 @@
+class MmsisController < ApplicationController
+  before_action :set_mmsi, only: %i[edit update destroy]
+
+  def dashboard
+    @stations = Mmsi.where(category: 'station')
+    @vessels = Mmsi.where(category: 'vessel')
+  end
+
+  def edit; end
+
+  def update
+    respond_to do |format|
+      if @mmsi.update(mmsi_params)
+        format.html { redirect_to dashboard_path, notice: 'Mmsi was successfully updated.', status: :see_other }
+        # format.json { render :show, status: :ok, location: @station }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        # format.json { render json: @station.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @mmsi.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to dashboard_path, notice: 'mmsi was successfully destroyed.', status: :see_other }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def set_mmsi
+    @mmsi = Mmsi.find(params.expect(:id))
+  end
+
+  def mmsi_params
+    params.expect(mmsi: [:status])
+  end
+end
