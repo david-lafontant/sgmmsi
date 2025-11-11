8641a9c58c8fccfbf7346b8d331032fbec54151c
david-lafontant <lafontant.david@gmail.com>
2025-11-11 07:33:01 -0500

    Set default status to false when creating callsign and define generate callsign method for Callsign model

--- app/models/callsign.rb
+++ app/models/callsign.rb
@@ -2,4 +2,16 @@ class Callsign < ApplicationRecord
   belongs_to :mmsi
   belongs_to :user
   has_many_attached :documents
+
+  after_initialize :set_default_status, if: :new_record?
+
+  private
+
+  def set_default_status
+    self[:status] ||= false
+  end
+
+  def generate_callsign
+    "call003290#{SecureRandom.random_number(10**5).to_s.rjust(5, '0')}"
+  end
 end
