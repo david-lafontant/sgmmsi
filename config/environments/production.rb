f522598b11fffe6681a6e15d5899e83d35a0f390
david-lafontant <lafontant.david@gmail.com>
2025-11-11 21:24:39 -0500

    Track delivery method error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,18 +55,20 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "https://sgmmsi-92d8b5274295.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: "sgmmsi-92d8b5274295.herokuapp.com" }
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     user_name: Rails.application.credentials.dig(:smtp, :user_name),
     password: Rails.application.credentials.dig(:smtp, :password),
     address: "smtp.gmail.com",
     port: 587,
-    authentication: :plain
+    authentication: :plain,
+    enable_starttls_auto: true
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
