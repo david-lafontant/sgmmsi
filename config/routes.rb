235f49a271fd9a0c80249f37a35527291a8468d5
david-lafontant <lafontant.david@gmail.com>
2025-11-12 19:57:35 -0500

    fix lintin errors in toutes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "pages#home"
   resources :callsigns
   resources :stations
   resources :station_types
@@ -6,7 +7,7 @@ Rails.application.routes.draw do
   resources :mmsis, only: [:edit, :update, :destroy]
   get '/dashboard', to: "mmsis#dashboard", as: :dashboard
   devise_for :users
-  root "pages#home"
+
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
