51e292f6ad48020b42e6011acaf1c1e50f954693
david-lafontant <lafontant.david@gmail.com>
2025-11-14 05:06:55 -0500

    Debug user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-  before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!
+  # before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :authenticate_user!
 
-  protected
+  # protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[role first_name last_name institution])
-  end
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: %i[role first_name last_name institution])
+  # end
 end
