59a0b7414bb717197d2e1c2d3cdc681a5b090df4
david-lafontant <lafontant.david@gmail.com>
2025-11-13 22:35:23 -0500

    Remove registerable from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :invitable, :database_authenticatable, :registerable,
+  devise :invitable, :database_authenticatable,
          :recoverable, :rememberable, :validatable, :confirmable, :lockable, :timeoutable
 
   enum :role, { user: 0, admin: 1 }.freeze
