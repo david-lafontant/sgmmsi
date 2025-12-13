b52ce7a1a70b3bd16d8eab186639d8de203183ad
david-lafontant <lafontant.david@gmail.com>
2025-12-12 20:59:18 -0500

    Remove users foreign key to vessels table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_12_13_011342) do
+ActiveRecord::Schema[8.0].define(version: 2025_12_13_015234) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -162,5 +162,4 @@ ActiveRecord::Schema[8.0].define(version: 2025_12_13_011342) do
   add_foreign_key "stations", "station_types"
   add_foreign_key "stations", "users"
   add_foreign_key "vessels", "mmsis"
-  add_foreign_key "vessels", "users"
 end
