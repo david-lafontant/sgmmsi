fa5daab1854c403add1ef2292a47973f616ed5d1
david-lafontant <lafontant.david@gmail.com>
2026-01-07 21:12:43 -0500

    Create shared method to find if user exists

---
+++ app/models/concerns/user_concern.rb
@@ -0,0 +1,10 @@
+module UserConcern
+  include ActiveSupport::Concern
+
+  def user_id_exists
+    User.find(user_id)
+  rescue ActiveRecord::RecordNotFound
+    errors.add(:user_id, 'user_id key must exist')
+    false
+  end
+end
