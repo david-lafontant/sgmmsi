8cf06272051b802283528c9679a36a84a9f07b1d
david-lafontant <lafontant.david@gmail.com>
2026-01-07 21:50:55 -0500

    Fix typo in parameter name in station_type_id_exists method

--- app/models/concerns/station_type_concern.rb
+++ app/models/concerns/station_type_concern.rb
@@ -2,9 +2,9 @@ module StationTypeConcern
   include ActiveSupport::Concern
 
   def station_type_id_exists
-    StationType.find(station_id)
+    StationType.find(station_type_id)
   rescue ActiveRecord::RecordNotFound
-    errors.add(:station_id, 'station_id key must exist')
+    errors.add(:station_type_id, 'station_type_id key must exist')
     false
   end
 end
