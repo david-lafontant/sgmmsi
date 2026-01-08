520387de222f91704cf66c2bab71c806baa2c844
david-lafontant <lafontant.david@gmail.com>
2026-01-07 21:23:10 -0500

    Validate the presence and format of category

--- app/models/station_type.rb
+++ app/models/station_type.rb
@@ -1,3 +1,6 @@
 class StationType < ApplicationRecord
   has_many :stations
+
+  string_regex = Regexp.new(/\A(?=.{3,40}\z)[A-Za-z-]+(?: [A-Za-z-]+)*\z/, Regexp::IGNORECASE)
+  validates :category, presence: true, format: { with: string_regex, message: 'invalid format' }
 end
