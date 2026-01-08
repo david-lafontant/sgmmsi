372cb44f74dbb162113fcf00e9bfa3d58339a3e7
david-lafontant <lafontant.david@gmail.com>
2026-01-08 13:07:30 -0500

    Define authorizations for each role

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,29 +4,22 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # Define abilities for the user here. For example:
-    #
-    #   return unless user.present?
-    #   can :read, :all
-    #   return unless user.admin?
-    #   can :manage, :all
-    #
-    # The first argument to `can` is the action you are giving the user
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, published: true
-    #
-    # See the wiki for details:
-    # https://github.com/CanCanCommunity/cancancan/blob/develop/docs/define_check_abilities.md
+    user ||= User.new
+
+    if user.admin?
+      can :manage, :all
+    elsif user.user?
+      can :read, :all
+      can %i[create update read], Vessel
+      can %i[create update read], Station
+      can %i[create read], Mmsi
+      can %i[create read], StationType
+      can %i[create read], Callsign
+      cannot :manage, User
+      cannot :invite, User
+    else
+      cannot :manage, :all
+      cannot :invite, User
+    end
   end
 end
