c312b40eba6636374976b19475216df0d0bdbf04
david-lafontant <lafontant.david@gmail.com>
2026-01-31 14:11:40 -0500

    Setup get_callsign method

--- app/models/concerns/callsign_concern.rb
+++ app/models/concerns/callsign_concern.rb
@@ -4,4 +4,8 @@ module CallsignConcern
   def generate_callsign
     "call003290#{SecureRandom.random_number(10**5).to_s.rjust(5, '0')}"
   end
+
+  def get_callsign(id)
+    Callsign.find_by(mmsi_id: id).call_sign_num
+  end
 end
