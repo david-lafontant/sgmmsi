7d888ec84253503c10bb70decabd90810bc2530e
david-lafontant <lafontant.david@gmail.com>
2026-02-10 10:11:48 -0500

    Trim and uppercase all the station type inputs

--- app/models/station_type.rb
+++ app/models/station_type.rb
@@ -3,4 +3,11 @@ class StationType < ApplicationRecord
 
   string_regex = Regexp.new(/\A(?=.{3,40}\z)[A-Za-z-]+(?: [A-Za-z-]+)*\z/, Regexp::IGNORECASE)
   validates :category, presence: true, format: { with: string_regex, message: 'invalid format' }
+  before_save :upcase_inputs
+
+  private
+
+  def upcase_inputs
+    self.category = category.strip.upcase
+  end
 end
