01084870c87689f806b823ced92e1c0c2d48d422
david-lafontant <lafontant.david@gmail.com>
2026-02-15 15:07:42 -0500

    Override active_for_authentication and innactive message to hanle banning user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,15 +6,24 @@ class User < ApplicationRecord
 
   enum :role, { user: 0, admin: 1 }.freeze
 
-  after_initialize :set_default_role, if: :new_record?
+  after_initialize :set_default_values, if: :new_record?
   has_many :stations
   has_many :mmsis
   has_many :vessels
   has_many :callsigns
 
+  def active_for_authentication?
+    super && !banned?
+  end
+
+  def inactive_message
+    banned? ? :banned : super
+  end
+
   private
 
-  def set_default_role
+  def set_default_values
     self.role ||= :user
+    self.banned ||= false
   end
 end
