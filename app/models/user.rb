c2260917fa58f3dc7fd2d8c2e6e643e4dbfa0e1c
david-lafontant <lafontant.david@gmail.com>
2026-02-16 17:19:16 -0500

    Define ransack attributes and association for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,6 +20,19 @@ class User < ApplicationRecord
     banned? ? :banned : super
   end
 
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[
+      email
+      first_name
+      last_name
+      institution
+    ]
+  end
+
+  def self.ransackable_associations(_auth_object = nil)
+    %w[mmsis callsigns vessels stations]
+  end
+
   private
 
   def set_default_values
