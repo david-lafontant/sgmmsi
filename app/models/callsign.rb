58523c63f860ff66e5d11ea33052f0ba8093c4e6
david-lafontant <lafontant.david@gmail.com>
2026-02-16 20:43:42 -0500

    Delete method to set default value for callsign status

--- app/models/callsign.rb
+++ app/models/callsign.rb
@@ -10,7 +10,6 @@ class Callsign < ApplicationRecord
 
   validate :user_id_exists
   validate :mmsi_id_exists
-  after_initialize :set_default_status, if: :new_record?
 
   def self.ransackable_attributes(_auth_object = nil)
     %w[
@@ -24,11 +23,5 @@ class Callsign < ApplicationRecord
 
   def self.ransackable_associations(_auth_object = nil)
     %w[mmsi user]
-  end
-
-  private
-
-  def set_default_status
-    self[:status] ||= true
   end
 end
